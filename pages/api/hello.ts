4a098cc14d105582228f4f2dc9500d0cc78f26d4
Dmytro Zharkov <dmytrozharkov@Noa.fritz.box>
2021-12-02 10:13:33 +0100

    Remove key

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import fetch from 'node-fetch';
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const rate = await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=milkywayex&CMC_PRO_API_KEY=xxx")
+  const data = await rate.json() as Data;
+  res.status(200).json(data)
 }
